f67a31f8e37d8a50ef3962af1434ea6f1c70b405
Vishal Sharma <vsh1819@gmail.com>
2018-06-08 13:19:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  array = []
+  names.each do |name|
+    array << "Hello, my name is #{name}."
+  end
+  return array
+end
+
+def assign_rooms(names)
+  names.each do |name|
+    
+  end
+end
